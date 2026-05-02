896fe1952877c3bd3807d34e02688057799ee1e1
abrshiz <abrshiz@yahoo.com>
2026-05-03 01:01:21 +0300

    feat: enhance resume analysis by integrating AI feedback and updating status messages

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -5,6 +5,7 @@ import Navbar from "~/components/Navbar"
 import { convertPdfToImage } from "~/lib/pdf2img";
 import { usePuterStore } from "~/lib/puter";
 import { generateUUID } from "~/lib/utils";
+import { prepareInstructions } from "../../constants";
 
 const Upload =()=>{
     const{auth,isLoading,fs, ai, kv} =usePuterStore();
@@ -43,6 +44,21 @@ const Upload =()=>{
             feedback:'',   
         }
         await kv.set(`resume:${uuid}`, JSON.stringify(data));
+        setStatusText('Analyzing the resume ...');
+
+        const feedback = await ai.feedback(
+            uploadedFile.path,
+            prepareInstructions({jobTitle, jobDescription})
+        )
+        if (!feedback) return setStatusText('Failed to analyze the resume. Please try again.');
+        const feedbackText= typeof feedback.message.content ==='string' 
+            ? feedback.message.content
+            : feedback.message.content[0].text;
+
+        data.feedback=JSON.parse(feedbackText);
+        await kv.set(`resume:${uuid}`, JSON.stringify(data));
+        setStatusText('Analysis complete! Redirecting to results page ...');
+        console.log(data);
     }
 
     const handleSubmit=(e: FormEvent<HTMLFormElement>)=>{
